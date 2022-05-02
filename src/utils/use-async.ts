65d8e3fab38f7c719d84b26ae68831c5b82e2d46
DDdlb <1032191254@qq.com>
2022-05-02 18:08:32 +0800

    update use-async using useReduce

--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,16 +1,24 @@
 import { useMountedRef } from './index';
-import { useCallback, useState } from 'react';
+import { useCallback, useReducer, useState } from 'react';
 interface State<D>{
     error: Error | null;
     data: D | null;
     stat: 'idle' | 'loading' | 'error' | 'success' // 异步操作未发生是idle，正在发生为loading，成功为success， 失败为error
 }
+// useReduce 适合会相互影响的状态
 
 const defaultInitialState: State<null> = {
     stat: 'idle',
     data: null,
     error: null
 }
+
+const useSafeDispatch = <T>(dispatch: (...args: T[])=>void)=>{
+    const mountedRef = useMountedRef()
+
+    return useCallback((...args: T[])=>(mountedRef.current? dispatch(...args): void 0), [dispatch, mountedRef])
+}
+
 // useState 传入函数，页面加载时会直接运行函数， 传入函数会被当作惰性初始化。
 // 惰性初始化：
 // const [state, setState] = useState(() => {
@@ -21,62 +29,128 @@ const defaultInitialState: State<null> = {
 // const [call, setCall] = useState(()=>()=>{})
 // 
 // 还可以用useRef存储函数, useRef值变化，组件不会更新
+
+// useAsync
+// export const useAsync = <D>(initialState?: State<D>)=>{
+//     const [state, setState] = useState<State<D>>({
+//         ...defaultInitialState,
+//         ...initialState
+//     })
+
+//     const mountedRef = useMountedRef()
+
+//     const [retry, setRetry] = useState(()=>()=>{})
+//     // 成功
+//     const setData = useCallback((data: D) => setState({
+//         data,
+//         stat: 'success',
+//         error: null
+//     }), [])
+//     // 失败
+//     const setError = (error: Error) =>setState({
+//         error,
+//         stat: 'error',
+//         data: null
+//     })
+//     // 用来触发异步请求, 防止run放到依赖中导致无限循环（每次创建的run视为不同函数）
+//     // const run = (promise: Promise<D>, runConfig?: {retry: ()=>Promise<D>}) =>{
+//     //     if(!promise || !promise.then){
+//     //         throw new Error('请传入Promise')
+//     //     }
+//     //     setState({
+//     //         ...state,
+//     //         stat: 'loading'
+//     //     })
+//     //     setRetry(()=>()=>{
+//     //         if(runConfig?.retry){
+//     //             run(runConfig?.retry(), runConfig)
+//     //         }
+//     //     })
+//     //     // 请求成功,返回请求数据
+//     //     return promise
+//     //         .then(data=>{
+//     //             if(mountedRef.current){
+//     //                 setData(data)
+//     //             }
+//     //             return data
+//     //         })
+//     //         .catch(error=>{
+//     //             setError(error)
+//     //             return error
+//     //         })
+//     //     // 
+//     // }
+
+//     // 使用 useCallBack 重写run
+//     const run = useCallback((promise: Promise<D>, runConfig?: {retry: ()=>Promise<D>}) =>{
+//             if(!promise || !promise.then){
+//                 throw new Error('请传入Promise')
+//             }
+//             setState({
+//                 ...state,
+//                 stat: 'loading'
+//             })
+//             setRetry(()=>()=>{
+//                 if(runConfig?.retry){
+//                     run(runConfig?.retry(), runConfig)
+//                 }
+//             })
+//             // 请求成功,返回请求数据
+//             return promise
+//                 .then(data=>{
+//                     if(mountedRef.current){
+//                         setData(data)
+//                     }
+//                     return data
+//                 })
+//                 .catch(error=>{
+//                     setError(error)
+//                     return error
+//                 })
+//             // 
+//         }, [mountedRef, setData, state])
+//     return {
+//         isIdle: state.stat === 'idle',
+//         isLoading: state.stat === 'loading',
+//         isError: state.stat === 'error',
+//         isSuccess: state.stat === 'success',
+//         setData,
+//         setError,
+//         run,
+//         retry,
+//         ...state
+//     }
+// }
+
+
+// useReducer改写
 export const useAsync = <D>(initialState?: State<D>)=>{
-    const [state, setState] = useState<State<D>>({
+    const [state, dispatch] = useReducer((state: State<D>, action: Partial<State<D>>)=>({...state, ...action}),{
         ...defaultInitialState,
         ...initialState
     })
 
-    const mountedRef = useMountedRef()
+    const safeDispatch = useSafeDispatch(dispatch)
 
     const [retry, setRetry] = useState(()=>()=>{})
     // 成功
-    const setData = useCallback((data: D) => setState({
+    const setData = useCallback((data: D) => safeDispatch({
         data,
         stat: 'success',
         error: null
-    }), [])
+    }), [safeDispatch])
     // 失败
-    const setError = (error: Error) =>setState({
+    const setError = useCallback((error: Error) =>safeDispatch({
         error,
         stat: 'error',
         data: null
-    })
-    // 用来触发异步请求, 防止run放到依赖中导致无限循环（每次创建的run视为不同函数）
-    // const run = (promise: Promise<D>, runConfig?: {retry: ()=>Promise<D>}) =>{
-    //     if(!promise || !promise.then){
-    //         throw new Error('请传入Promise')
-    //     }
-    //     setState({
-    //         ...state,
-    //         stat: 'loading'
-    //     })
-    //     setRetry(()=>()=>{
-    //         if(runConfig?.retry){
-    //             run(runConfig?.retry(), runConfig)
-    //         }
-    //     })
-    //     // 请求成功,返回请求数据
-    //     return promise
-    //         .then(data=>{
-    //             if(mountedRef.current){
-    //                 setData(data)
-    //             }
-    //             return data
-    //         })
-    //         .catch(error=>{
-    //             setError(error)
-    //             return error
-    //         })
-    //     // 
-    // }
-
+    }), [safeDispatch])
     // 使用 useCallBack 重写run
     const run = useCallback((promise: Promise<D>, runConfig?: {retry: ()=>Promise<D>}) =>{
             if(!promise || !promise.then){
                 throw new Error('请传入Promise')
             }
-            setState({
+            safeDispatch({
                 ...state,
                 stat: 'loading'
             })
@@ -88,9 +162,9 @@ export const useAsync = <D>(initialState?: State<D>)=>{
             // 请求成功,返回请求数据
             return promise
                 .then(data=>{
-                    if(mountedRef.current){
-                        setData(data)
-                    }
+                    
+                    setData(data)
+                    
                     return data
                 })
                 .catch(error=>{
@@ -98,7 +172,7 @@ export const useAsync = <D>(initialState?: State<D>)=>{
                     return error
                 })
             // 
-        }, [mountedRef, setData, state])
+        }, [setData, state, safeDispatch, setError])
     return {
         isIdle: state.stat === 'idle',
         isLoading: state.stat === 'loading',
--- src/utils/use-undo.ts
+++ src/utils/use-undo.ts
@@ -1,36 +1,29 @@
-import { useState, useCallback } from 'react';
+import { useCallback, useReducer } from 'react';
 
 /**
  *  用来记录操作历史
  * @param initialPresent 
  */
-export const useUndo = <T>(initialPresent: T)=>{
-    // // past 用来记录历史操作的合集（后退）
-    // const [past, setPast] = useState<T[]>([])
 
-    // const [present, setPresent] = useState(initialPresent)
-    // // 未来操作(前进)
-    // const [future, setFuture] = useState<T[]>([])
+const UNDO = 'UNDO'
+const REDO = 'REDO'
+const SET = 'SET'
+const RESET = 'RESET'
 
-    // 状态合并
-    const [state, setState] = useState<{
-        past: T[],
-        present: T,
-        future: T[]
-    }>({
-        past: [],
-        present: initialPresent,
-        future: []
-    })
-    // 是否可后退
-    const canUndo = state.past.length !== 0
-    // 是否可前进
-    const canRedo = state.future.length !== 0
-    // 后退操作  使用useCallback防止重复渲染死循环
-    const undo = useCallback(()=>{
-        setState(currentState=> {
-            const {past, present, future} = currentState
-            if(past.length === 0)return currentState
+type State<T> = {
+    past: T[],
+    present: T,
+    future: T[]
+}
+
+type Action<T> = {newPresent?: T, type: typeof UNDO | typeof REDO | typeof SET | typeof RESET}
+
+const undoReducer = <T>(state: State<T>, action: Action<T>) =>{
+    const {past, present, future} = state
+    const {newPresent} = action
+    switch(action.type){
+        case UNDO: {
+            if(past.length === 0)return state
 
             const previous = past[past.length - 1]
             const newPast = past.slice(0, past.length - 1)
@@ -40,13 +33,10 @@ export const useUndo = <T>(initialPresent: T)=>{
                 present: previous,
                 future: [present, ...future]
             }
-        })
-    }, [])
-    // 前进操作
-    const redo = useCallback(()=>{
-        setState(currentState=>{
-            const {past, present, future} = currentState
-            if(future.length === 0) return currentState
+        }
+
+        case REDO: {
+            if(future.length === 0) return state
 
             const next = future[0]
             const newFuture = future.slice(1)
@@ -56,31 +46,64 @@ export const useUndo = <T>(initialPresent: T)=>{
                 present: next,
                 future: newFuture
             }
-        })
-    }, [])
-    // 改变当前值
-    const set = useCallback((newPresent: T) => {
-        setState(currentState=>{
-            const {past, present} = currentState
-            if(newPresent === present) return currentState
+        }
+
+        case SET: {
+            if(newPresent === present) return state
 
             return {
                 past: [...past, present],
                 present: newPresent,
                 future: []
             }
-        }) 
-    }, [])
-    // 重制历史
-    const reset = useCallback((newPresent: T)=>{
-        setState(currentState=>{
+        }
+
+        case RESET: {
             return {
                 past: [],
                 present: newPresent,
                 future: []
             }
-        })
-    }, [])
+        }
+    }
+}
+export const useUndo = <T>(initialPresent: T)=>{
+    // // past 用来记录历史操作的合集（后退）
+    // const [past, setPast] = useState<T[]>([])
+
+    // const [present, setPresent] = useState(initialPresent)
+    // // 未来操作(前进)
+    // const [future, setFuture] = useState<T[]>([])
+
+    // 状态合并
+    // const [state, setState] = useState<{
+    //     past: T[],
+    //     present: T,
+    //     future: T[]
+    // }>({
+    //     past: [],
+    //     present: initialPresent,
+    //     future: []
+    // })
+
+    // reducer实现
+    const [state, dispatch] = useReducer(undoReducer, {
+        past: [],
+        present: initialPresent,
+        future: []
+    } as State<T>)
+    // 是否可后退
+    const canUndo = state.past.length !== 0
+    // 是否可前进
+    const canRedo = state.future.length !== 0
+    // 后退操作  使用useCallback防止重复渲染死循环
+    const undo = useCallback(()=> dispatch({type: UNDO}), [])
+    // 前进操作
+    const redo = useCallback(()=>dispatch({type: REDO}), [])
+    // 改变当前值
+    const set = useCallback((newPresent: T) => dispatch({newPresent, type: SET}), [])
+    // 重制历史
+    const reset = useCallback((newPresent: T)=> dispatch({newPresent, type: RESET}), [])
 
     return [
         state,
